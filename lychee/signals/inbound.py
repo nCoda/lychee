4a2a8a08294ae34a71702229c75c8ad2507a3c29
Christopher Antila <christopher@antila.ca>
2015-06-11 13:56:29 -0400

    rewrote the "inbound" signals example
    I think this is much more intelligent, with a single object to manage the
    control flow, rather than relying on every module to always work and participate
    correctly. Seemed like the previous idea would lead to some dirty hacks.

--- lychee/signals/inbound.py
+++ lychee/signals/inbound.py
@@ -30,6 +30,11 @@ CONVERSION_FINISHED = signalslot.Signal()
 Emitted when the inbound conversion is finished, before any "views" information is processed.
 '''
 
+CONVERSION_ERROR = signalslot.Signal()
+'''
+Emitted when there's an error during the in bound conversion step.
+'''
+
 VIEWS_START = signalslot.Signal()
 '''
 Emitted when the inbound view processing will start (i.e., this signal is emitted to cause the views
@@ -53,26 +58,135 @@ VIEWS_FINISHED = signalslot.Signal()
 Emitted when the inbound views processing is finished.
 '''
 
+VIEWS_ERROR = signalslot.Signal()
+'''
+Emitted when there's an error while processing the inbound view.
+'''
+
+
+#--------------------------------------------------------------------------------------------------#
+# Set up the "inbound" step's workflow.                                                            #
+#--------------------------------------------------------------------------------------------------#
+class WorkflowManager(object):
+    '''
+    It manages the workflow.
+    '''
+    # TODO: move this into its own module; should it be a submodule of "signals" or not?
+    # TODO: add support for the error states
+
+    # statusses
+    _PRESTART = 0
+    _INBOUND_PRECONVERT = 1
+    _INBOUND_CONVERT = 2
+    _INBOUND_CONVERT_FAIL = 3
+    _INBOUND_CONVERT_FINISH = 4
+    _INBOUND_PREVIEWS = 5
+    _INBOUND_VIEWS = 6
+    _INBOUND_VIEWS_FAIL = 7
+    _INBOUND_VIEWS_FINISH = 8
+
+    # connections
+    # NB: they'll be connected as SIGNAL.connect(self.whatever)
+    _CONNECTIONS = ((CONVERSION_STARTED, '_inbound_conversion_started'),
+                    (CONVERSION_FINISH, '_inbound_conversion_finish'),
+                    (CONVERSION_FINISHED, '_inbound_conversion_finished'),
+                    (VIEWS_STARTED, '_inbound_views_started'),
+                    (VIEWS_FINISH, '_inbound_views_finish'),
+                    (VIEWS_FINISHED, '_inbound_views_finished'),
+                   )
+
+    def __init__(self, kwargs):
+        self._status = WorkflowManager._PRESTART
+        self._kwargs = kwargs
+        for signal, slot in WorkflowManager._CONNECTIONS:
+            signal.connect(getattr(self, slot))
+
+    def run(self):
+        '''
+        Runs the "action" as required.
+        '''
+        if self._status is not WorkflowManager._PRESTART:
+            print('failure starting the action')
+            return
+
+        self._status = WorkflowManager._INBOUND_PRECONVERT
+        self._choose_inbound_converter()
+        CONVERSION_START.emit()
+
+        if self._status is not WorkflowManager._INBOUND_CONVERT_FINISH:
+            print('failure during inbound conversion')
+            return
+
+        self._status = WorkflowManager._INBOUND_PREVIEWS
+        self._choose_inbound_views()
+        VIEWS_START.emit()
+
+    def _choose_inbound_converter(self):
+        '''
+        Choose an inbound converter based on self._kwargs.
+        '''
+        CONVERSION_START.connect(mock_converter)
+
+    def _choose_inbound_views(self):
+        '''
+        Choose an inbound views function based on self._kwargs and the conversion result.
+        '''
+        VIEWS_START.connect(mock_views)
+
+    def _inbound_conversion_started(self, **kwargs):
+        print('inbound conversion started')
+        self._status = WorkflowManager._INBOUND_CONVERT
+
+    def _inbound_conversion_finish(self, **kwargs):
+        print('inbound conversion finishing')
+        self._status = WorkflowManager._INBOUND_CONVERT_FINISH
+        CONVERSION_FINISHED.emit()
+
+    def _inbound_conversion_finished(self, **kwargs):
+        '''
+        Called when inbound.CONVERSION_FINISHED is emitted, for logging and debugging.
+        '''
+        print('inbound conversion finished')
+
+    def _inbound_views_started(self, **kwargs):
+        print('inbound views started')
+        self._status = WorkflowManager._INBOUND_VIEWS
+
+    def _inbound_views_finish(self, **kwargs):
+        print('inbound views finishing'.format(kwargs))
+        self._status = WorkflowManager._INBOUND_VIEWS_FINISH
+        VIEWS_FINISHED.emit()
+
+    def _inbound_views_finished(self, **kwargs):
+        '''
+        Called when inbound.VIEWS_FINISHED is emitted, for logging and debugging.
+        '''
+        print('inbound views finished')
+
+
 #--------------------------------------------------------------------------------------------------#
 # Set up the "inbound" step's workflow.                                                            #
 #--------------------------------------------------------------------------------------------------#
 
+# mocks of other modules
 def mock_converter(**kwargs):
     CONVERSION_STARTED.emit()
     print('mock_converter({})'.format(kwargs))
     CONVERSION_FINISH.emit()
+    print('mock_converter() after finish signal')
 
-def conv_st(**kwargs):
-    print('conversion started')
+def mock_views(**kwargs):
+    VIEWS_STARTED.emit()
+    print('mock_views({})'.format(kwargs))
+    VIEWS_FINISH.emit()
+    print('mock_views() after finish signal')
 
-def conv_fin(**kwargs):
-    print('conversion finishing')
+# actual signal connections
+def thing(**kwargs):
+    workm = WorkflowManager(kwargs)
+    workm.run()
 
+ACTION_START.connect(thing)
 
-ACTION_START.connect(CONVERSION_START.emit)
-CONVERSION_START.connect(mock_converter)
-CONVERSION_STARTED.connect(conv_st)
-CONVERSION_FINISH.connect(conv_fin)
-
-
+# this is what starts a test "action"
 ACTION_START.emit()

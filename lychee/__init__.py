b0566b78dffe3778a6b573211599f8fbf95cdfd2
Christopher Antila <christopher@antila.ca>
2015-07-05 17:04:57 -0400

    New: setup.py file, so "lychee" is installable, for easier development
    If you run "pip install -e ." in the Lychee directory (with setup.py), then you
    won't have to worry about your PYTHONPATH.

--- lychee/__init__.py
+++ lychee/__init__.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+__version__ = '0.0.1'
+
 __all__ = ['converters', 'signals', 'tui', 'vcs', 'views']
 
 from lychee import *
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from setuptools import setup, Command
+import lychee  # for __version__
+
+
+setup(
+    name = 'Lychee',
+    version = lychee.__version__,
+    packages = ['lychee'],
+
+    install_requires = ['signalslot==0.0.11'],
+
+    # metadata for upload to PyPI
+    author = 'Christopher Antila',
+    author_email = 'christopher@antila.ca',
+    description = 'The tastiest fruit around!',
+    license = 'GPLv3+',
+    url = 'http://ncodamusic.org/lychee',
+    # TODO: keywords, long_description, download_url, classifiers, etc.
+)

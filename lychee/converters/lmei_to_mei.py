6384978ee2348484f57962fd2e7acdc31978141b
Christopher Antila <christopher@antila.ca>
2015-07-07 15:50:32 -0400

    New: barebones LMEI-to-MEI converter
    For now it doesn't really do anything but make a <section> into a properly
    completed document.

--- lychee/converters/lmei_to_mei.py
+++ lychee/converters/lmei_to_mei.py
@@ -26,19 +26,47 @@
 Converts a Lychee-MEI document to a more conventional document.
 '''
 
+from lxml import etree as ETree
+
 from lychee.signals import outbound
 
-def convert(**kwargs):
+
+_MEINS = '{http://www.music-encoding.org/ns/mei}'
+
+
+def convert(document, **kwargs):
     '''
     Convert a Lychee-MEI document into an MEI document.
 
-    :param document: The Lychee-MEI document. Must be provided as a kwarg.
+    :param document: The Lychee-MEI document.
     :type document: :class:`xml.etree.ElementTree.Element` or :class:`xml.etree.ElementTree.ElementTree`
     :returns: The corresponding MEI document.
     :rtype: :class:`xml.etree.ElementTree.Element` or :class:`xml.etree.ElementTree.ElementTree`
     '''
     outbound.CONVERSION_STARTED.emit()
-    print('{}.convert(document="{}")'.format(__name__, kwargs['document']))
-    #outbound.CONVERSION_ERROR.emit()
-    outbound.CONVERSION_FINISH.emit(converted='<mei stuff>')
+    print('{}.convert(document={})'.format(__name__, document))
+
+    if '{}section'.format(_MEINS) != document.tag:
+        outbound.CONVERSION_ERROR.emit(msg='LMEI-to-MEI did not receive a <section>')
+        return
+
+    scoreDef = ETree.Element('{}scoreDef'.format(_MEINS))
+    staffGrp = ETree.Element('{}staffGrp'.format(_MEINS), attrib={'symbol': 'line'})
+    staffDef = ETree.Element('{}staffDef'.format(_MEINS), attrib={'n': '1', 'lines': '5'})
+    staffGrp.append(staffDef)
+    scoreDef.append(staffGrp)
+    document.insert(0, scoreDef)
+
+    score = ETree.Element('{}score'.format(_MEINS))
+    score.append(document)
+    mdiv = ETree.Element('{}mdiv'.format(_MEINS))
+    mdiv.append(score)
+    body = ETree.Element('{}body'.format(_MEINS))
+    body.append(mdiv)
+    music = ETree.Element('{}music'.format(_MEINS))
+    music.append(body)
+    mei = ETree.Element('{}mei'.format(_MEINS))
+    mei.append(music)
+
+    outbound.CONVERSION_FINISH.emit(converted=mei)
     print('{}.convert() after finish signal'.format(__name__))

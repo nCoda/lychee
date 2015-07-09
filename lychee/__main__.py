ceb7582a2c0f4ae1c99b1657c9e6f849110c94d0
Christopher Antila <christopher@antila.ca>
2015-07-09 19:14:28 -0400

    lychee.__main__ now has an Abjad test in it

--- lychee/__main__.py
+++ lychee/__main__.py
@@ -38,6 +38,12 @@ _MEINS = '{http://www.music-encoding.org/ns/mei}'
 _MEINS_URL = 'http://www.music-encoding.org/ns/mei'
 
 
+test_which_format = None
+# NOTE: if you're running Lychee as a test program, you must uncomment one of these
+test_which_format = 'lilypond'
+#test_which_format = 'abjad'
+
+
 # register these fake "listeners" that will pretend they want data in whatever formats
 def generic_listener(dtype):
     lychee.log("I'm listening for {}!".format(dtype))
@@ -82,5 +88,22 @@ outbound.WHO_IS_LISTENING.connect(mei_listener)
 outbound.CONVERSION_FINISHED.connect(mei_through_verovio)
 
 # this is what starts a test "action"
-input_ly = "\clef treble a''4( b'16 c''2)  | \clef \"bass\" d?2 e!2  | f,,2( fis,2)  |"
-signals.ACTION_START.emit(dtype='LilyPond', doc=input_ly)
+if 'lilypond' == test_which_format:
+    input_ly = "\clef treble a''4( b'16 c''2)  | \clef \"bass\" d?2 e!2  | f,,2( fis,2)  |"
+    signals.ACTION_START.emit(dtype='LilyPond', doc=input_ly)
+elif 'abjad' == test_which_format:
+    from abjad import *
+    s = Voice("a''4 r4 <a'' b''> b'' a''4. r4.")
+    a = Voice("d' d' d' d' d' cs' d' d'")
+    top = Staff([s, a])
+    top.is_simultaneous = True
+    t = Voice("fs''  fs''8 g'' a''4  g''8 fs''  e'' d'' e''4  d'' d''")
+    b = Voice("d'4 b' fs' g' a' a' d' d'")
+    bottom = Staff([t,b])
+    bottom.is_simultaneous = True
+    group = StaffGroup([top,bottom])
+    tuba = Staff("c''''1 r c''''")
+    score = Score([group,tuba])
+    signals.ACTION_START.emit(dtype='abjad', doc=score)
+else:
+    raise RuntimeError('you must choose a format in lychee.__main__')

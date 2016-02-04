ac5e8abd8d56232976bd257f4aa31358d4d5949c
Christopher Antila <christopher@antila.ca>
2016-02-04 00:24:53 -0500

    Fixed: rST formatting of MEI header sample in lychee.document
    I just wrote the sample XML into the docstring, but it's a code sample, so
    it should have been preceded by :: . So now it is.

--- lychee/document/document.py
+++ lychee/document/document.py
@@ -34,84 +34,87 @@ and they are included in the header returned by :func:`document._empty_head` wit
 
 .. note:: Additional metadata will very likely be added in the future.
 
-<meiHead>
-    <fileDesc>  <!-- required -->
-        <titleStmt>  <!-- required -->
-            <!-- NB: having all title parts in a containing <title>, and using the @type attribute,
-                 are required for Lychee-MEI, and optional in standard MEI
-                 NB: the @xml:lang and @translit are optional, and their use will be specified later
-            -->
-            <title @xml:lang="??" @translit="?"> <!-- required -->
-                <title type="main"></title>  <!-- required -->
-                <title type="subordinate"></title>
-                <title type="abbreviated"></title>
-                <title type="alternative"></title>
-                <title type="translated"></title>
-                <title type="uniform"></title>
-            </title>
-            <!-- NB: the following <titleStmt> child elements are not required -->
-            <respStmt>
-                <!-- this is for users who worked on the document -->
-                <!-- users may wish to credit Abjad; translatable string -->
-                <name type="process">Abjad API for Formalized Score Control</name>
-                <persName @xml:id="p8109850029">
-                    <!-- NB: use as many of the @type="full", @type="given", @type="other", and
-                         @type="family" child elements as possible, according to what the person
-                         responsible wishes
-                         NB: the @xml:lang and @translit are optional, and will be specified later
-                    -->
-                    <persName type=""></persName>
-                </persName>
-            </respStmt>
-            <!-- NB: the following are what is written on the engraved score; if they correspond to
-                 a person in the <respStmt>, this should be done with a @nymref on the <persName>
-            -->
-            <arranger>
-                <!-- for an arranger who isn't a Lychee user -->
-                <persName xml:id="p12341234">
-                    <persName type="full">Robert W. Smith</persName>
-                </persName>
-            </arranger>
-            <author>
-                <!-- for an author who is a Lychee user -->
-                <persName nymref="#p8109850029"/>
-            </author>
-            <composer><persName/></composer>
-            <editor><persName/></editor>
-            <funder><persName/></funder>
-            <librettist><persName/></librettist>
-            <lyricist><persName/></lyricist>
-            <sponsor><persName/></sponsor>
-        </titleStmt>
+::
 
-        <pubStmt>  <!-- required -->
-            <!-- NB: all Lychee scores are considered unpublished for now -->
-            <unpub>  <!-- required; text content is translatable -->
-                This is an unpublished Lychee-MEI document.
-            </unpub>
-        </pubStmt>
-    </fileDesc>
+    <meiHead>
+        <fileDesc>  <!-- required -->
+            <titleStmt>  <!-- required -->
+                <!-- NB: having all title parts in a containing <title>, and using the @type attribute,
+                     are required for Lychee-MEI, and optional in standard MEI
+                     NB: the @xml:lang and @translit are optional, and their use will be specified later
+                -->
+                <title @xml:lang="??" @translit="?"> <!-- required -->
+                    <title type="main"></title>  <!-- required -->
+                    <title type="subordinate"></title>
+                    <title type="abbreviated"></title>
+                    <title type="alternative"></title>
+                    <title type="translated"></title>
+                    <title type="uniform"></title>
+                </title>
+                <!-- NB: the following <titleStmt> child elements are not required -->
+                <respStmt>
+                    <!-- this is for users who worked on the document -->
+                    <!-- users may wish to credit Abjad; translatable string -->
+                    <name type="process">Abjad API for Formalized Score Control</name>
+                    <persName @xml:id="p8109850029">
+                        <!-- NB: use as many of the @type="full", @type="given", @type="other", and
+                             @type="family" child elements as possible, according to what the person
+                             responsible wishes
+                             NB: the @xml:lang and @translit are optional, and will be specified later
+                        -->
+                        <persName type=""></persName>
+                    </persName>
+                </respStmt>
+                <!-- NB: the following are what is written on the engraved score; if they correspond to
+                     a person in the <respStmt>, this should be done with a @nymref on the <persName>
+                -->
+                <arranger>
+                    <!-- for an arranger who isn't a Lychee user -->
+                    <persName xml:id="p12341234">
+                        <persName type="full">Robert W. Smith</persName>
+                    </persName>
+                </arranger>
+                <author>
+                    <!-- for an author who is a Lychee user -->
+                    <persName nymref="#p8109850029"/>
+                </author>
+                <composer><persName/></composer>
+                <editor><persName/></editor>
+                <funder><persName/></funder>
+                <librettist><persName/></librettist>
+                <lyricist><persName/></lyricist>
+                <sponsor><persName/></sponsor>
+            </titleStmt>
 
-    <workDesc>  <!-- (NB: not yet implemented) -->
-        <work>
-            <audience/  <!-- e.g., "beginner bands" (NB: not yet implemented) -->
-            <classification/>  <!-- like "keywords" (NB: not yet implemented) -->
-            <contents/>  <!-- a description of doc contents (NB: not yet implemented) -->
-            <context/>  <!-- socio-historical context (NB: not yet implemented) -->
-            <history/>  <!-- (NB: not yet implemented) -->
-            <key pname="" accid="" mode=""/>  <!-- (NB: not yet implemented) -->
-            <langUsage/>  <!-- related to @xml:lang elsewhere (NB: not yet implemented) -->
-            <mensuration/>  <!-- (NB: not yet implemented) -->
-            <meter count="" sym="" unit=""/>  <!--  (NB: not yet implemented) -->
-            <notesStmt/>  <!-- for score-wide notes left by users (NB: not yet implemented) -->
-            <perfMedium/>  <!-- intened performers of this version (NB: not yet implemented) -->
-        </work>
-    </workDesc>
+            <pubStmt>  <!-- required -->
+                <!-- NB: all Lychee scores are considered unpublished for now -->
+                <unpub>  <!-- required; text content is translatable -->
+                    This is an unpublished Lychee-MEI document.
+                </unpub>
+            </pubStmt>
+        </fileDesc>
 
-    <revisionDesc>
-        <!-- NB: not yet implemented -->
-    </revisionDesc>
-</meiHead>
+        <workDesc>  <!-- (NB: not yet implemented) -->
+            <work>
+                <audience/  <!-- e.g., "beginner bands" (NB: not yet implemented) -->
+                <classification/>  <!-- like "keywords" (NB: not yet implemented) -->
+                <contents/>  <!-- a description of doc contents (NB: not yet implemented) -->
+                <context/>  <!-- socio-historical context (NB: not yet implemented) -->
+                <history/>  <!-- (NB: not yet implemented) -->
+                <key pname="" accid="" mode=""/>  <!-- (NB: not yet implemented) -->
+                <langUsage/>  <!-- related to @xml:lang elsewhere (NB: not yet implemented) -->
+                <mensuration/>  <!-- (NB: not yet implemented) -->
+                <meter count="" sym="" unit=""/>  <!--  (NB: not yet implemented) -->
+                <notesStmt/>  <!-- for score-wide notes left by users (NB: not yet implemented) -->
+                <perfMedium/>  <!-- intened performers of this version (NB: not yet implemented) -->
+            </work>
+        </workDesc>
+
+        <revisionDesc>
+            <!-- NB: not yet implemented -->
+        </revisionDesc>
+    </meiHead>
+
 '''
 
 import os.path
